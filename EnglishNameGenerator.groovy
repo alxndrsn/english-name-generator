33ed341e7bf15c887e1eb4dc00c5ee1c797f415d
Alex Anderson <alex@frontlinesms.com>
2012-12-07 18:08:24 +0300

    Added titles to English Names.

--- EnglishNameGenerator.groovy
+++ EnglishNameGenerator.groovy
@@ -1,2 +1,3 @@
 def p = 'and marc son bert wick ness ton shire step ley ing sley'.split()
-((0..2).collect { p[new Random().nextInt(p.size())] }.join('').capitalize() + ' ' + (0..7).collect { p[new Random().nextInt(p.size())] }.join('').capitalize())
+def q = 'Lord Lady Viscount Baronet Marquis Sir Captain Admiral'.split()
+println q[new Random().nextInt(q.size())] + ' ' + (0..2).collect { p[new Random().nextInt(p.size())] }.join('').capitalize() + ' ' + (0..7).collect { p[new Random().nextInt(p.size())] }.join('').capitalize()
