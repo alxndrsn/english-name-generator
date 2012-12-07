9dedea547ecdaa513de872948d90b8eb6d2e5d23
Alex Anderson <alex@frontlinesms.com>
2012-12-07 18:03:18 +0300

    Added some more syllables

--- swa_gen.groovy
+++ swa_gen.groovy
@@ -2,7 +2,7 @@ class SwahiliGenerator {
 	def r = new Random()
 
 	def word = {
-	    def p = 'ka ku m i mw na ni ta tu wa wi me mi si shi pa pe li la lei'.split()
+	    def p = 'ka ku kwe m i u hu mw na ni ta tu wa wi me mi si shi pa pe li la lei'.split()
 	    (0..(1 + r.nextInt(5))).collect { p[r.nextInt(p.size())] }.join('') + ' '
 	}
 
